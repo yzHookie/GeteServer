58feff653de1bb4a6fa94b9def74719e013a9f53
hzy <hzy0052@foxmail.com>
2024-11-10 18:32:20 +0800

    fix:更新因protobuf版本问题导致的编译错误

--- proto/gen_protobuf.py
+++ proto/gen_protobuf.py
@@ -7,17 +7,23 @@ def gen_protobuf(proto_file):
     proto_dir = os.path.dirname(os.path.abspath(__file__))
 
     # 构建 protoc 命令
-    protoc_command = (
+    protoc_command_0 = (
         f'protoc.exe -I="{proto_dir}" --grpc_out="{proto_dir}" '
         f'--plugin=protoc-gen-grpc="C:\\env\\vcpkg\\packages\\grpc_x64-windows\\tools\\grpc\\grpc_cpp_plugin.exe" '
         f'"{proto_file}"'
     )
 
     # 打印命令以便调试
-    print(f"Executing command: {protoc_command}")
+    print(f"Executing command: {protoc_command_0}")
 
     # 执行命令
-    os.system(protoc_command)
+    os.system(protoc_command_0)
+
+    protoc_command_1 = f'protoc.exe --cpp_out="{proto_dir}" ' f'"{proto_file}"'
+
+    # 打印命令以便调试
+    print(f"Executing command: {protoc_command_1}")
+    os.system(protoc_command_1)
 
 
 if __name__ == "__main__":
